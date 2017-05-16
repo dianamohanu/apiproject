0b18549bc1d51b718e63862d808a3c1368c7e0df
dianamarin28 <diana.marinmohanu@gmail.com>
2017-05-16 15:45:37 +0300

    security config

--- src/main/java/com/myproject/mvc/HomeController.java
+++ src/main/java/com/myproject/mvc/HomeController.java
@@ -1,17 +1,21 @@
 package com.myproject.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController
 {
-
 	@RequestMapping(value = "", method = RequestMethod.GET)
-	public String home()
-	{
-		return "test";
+	public String homePage() {
+		return "redirect:/backoffice";
+	}
+
+	@RequestMapping(value = "/backoffice", method = RequestMethod.GET)
+	public String backofficeHome(Model model) {
+		return "backoffice";
 	}
 
 }
